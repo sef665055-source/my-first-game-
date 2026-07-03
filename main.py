463e66c90b0ead96d50b1aead9ffd56db663ec76
sef665055-source <sef665055@gmail.com>
2026-07-03 17:28:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ jobs:
     steps:
       - uses: actions/checkout@v4
 
+      - name: Set up Python
+        uses: actions/setup-python@v5
+        with:
+          python-version: '3.10'
+
+      - name: Install Buildozer to generate spec file
+        run: |
+          pip install buildozer
+          buildozer init
+
       - name: Build APK with Buildozer Action
         uses: ArtemGovorov/buildozer-action@v1
         with:
